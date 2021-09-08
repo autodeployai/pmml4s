9f7938e078e75af7ddca314bbd8f67f4a650b078
scorebot <scorebot@outlook.com>
2021-09-08 15:13:49 +0000

    return default result when no rule found

--- src/main/scala/org/pmml4s/model/RuleSetModel.scala
+++ src/main/scala/org/pmml4s/model/RuleSetModel.scala
@@ -55,7 +55,9 @@ class RuleSetModel(override var parent: Model,
       return nullSeries
     }
 
-    val outputs = createOutputs
+    val outputs = createOutputs.
+      setPredictedValue(ruleSet.defaultScore.orNull).
+      setConfidence(ruleSet.defaultConfidence.getOrElse(Double.NaN))
 
     import Criterion._
     criterion match {
